0d7a0cd000d347a90b166a97293e5aeb1dce7882
crlsmcd <crlsmcd@gmail.com>
2023-02-13 16:15:06 +0000

    Atualização pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,11 @@
-input.onButtonPressed(Button.A, function () {
-    basic.showNumber(56)
-})
-input.onButtonPressed(Button.AB, function () {
-    basic.showIcon(IconNames.Heart)
-})
-input.onButtonPressed(Button.B, function () {
-    basic.showString("Hello!")
+let mao = 0
+input.onGesture(Gesture.Shake, function () {
+    mao = randint(1, 3)
+    if (mao == 1) {
+        basic.showIcon(IconNames.SmallSquare)
+    } else if (mao == 2) {
+        basic.showIcon(IconNames.Square)
+    } else {
+        basic.showIcon(IconNames.Scissors)
+    }
 })
